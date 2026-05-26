cbba67fc11bc43791e4892ad00875faea75061a1
ArmV17 <22040111@alumno.utc.edu.mx>
2026-05-25 21:56:57 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'logistica-abc',
+  webDir: 'dist'
+};
+
+export default config;
